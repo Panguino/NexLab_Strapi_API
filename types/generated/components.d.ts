faa80b19b48467cc25bce350a6423a41c9affefc
Evan Anderson <elusiveuphoria@gmail.com>
2024-01-22 13:16:34 -0600

    Attempting degrees

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,41 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface InfoInfoGroup extends Schema.Component {
+  collectionName: 'components_info_info_groups';
+  info: {
+    displayName: 'InfoGroup';
+    icon: 'dashboard';
+    description: '';
+  };
+  attributes: {
+    Title: Attribute.String;
+    MoreInfoLink: Attribute.Component<'info.more-info'>;
+    Description: Attribute.Component<'info.info'>;
+  };
+}
+
+export interface InfoInfo extends Schema.Component {
+  collectionName: 'components_info_infos';
+  info: {
+    displayName: 'Info';
+    icon: 'file';
+  };
+  attributes: {
+    Details: Attribute.Text;
+  };
+}
+
+export interface InfoMoreInfo extends Schema.Component {
+  collectionName: 'components_info_more_infos';
+  info: {
+    displayName: 'MoreInfo';
+    icon: 'link';
+  };
+  attributes: {
+    MoreInfoLink: Attribute.String;
+  };
+}
+
 export interface ListMaterialGroup extends Schema.Component {
   collectionName: 'components_list_material_groups';
   info: {
@@ -30,6 +66,9 @@ export interface ListMaterials extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'info.info-group': InfoInfoGroup;
+      'info.info': InfoInfo;
+      'info.more-info': InfoMoreInfo;
       'list.material-group': ListMaterialGroup;
       'list.materials': ListMaterials;
     }
