03b8f1e3043b554dfad9088149b113d0e197ac97
Brad Larson <brad@bannerwave.com>
2025-09-16 07:04:54 -0400

    cke fix

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -53,7 +53,14 @@ export interface BlocksFeatureData extends Struct.ComponentSchema {
   attributes: {
     data_info_panels: Schema.Attribute.Component<'list.data-info-panel', true>;
     intro_text: Schema.Attribute.RichText &
-      Schema.Attribute.CustomField<'plugin::ckeditor.CKEditor'>;
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor.CKEditor',
+        {
+          licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3ODE5MTM1OTksImp0aSI6IjAwNzQ3NzFlLTljMTEtNDZiMC04MDNkLWQ0NDc4Y2RlZjI1NiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCIsIkUyUCIsIkUyVyJdLCJ2YyI6ImU2YWIwMzhhIn0.GpIsNENbfZ9D4_CL1Go1xs7h8ycVnXq-rrQLj3c-h4do2kxO5s3S7PfPSn0J2Yg-01PmRsSmyuFPUCB4zakwAQ';
+          output: 'HTML';
+          preset: 'rich';
+        }
+      >;
   };
 }
 
@@ -146,7 +153,14 @@ export interface BlocksSimpleCta extends Struct.ComponentSchema {
     background_full: Schema.Attribute.Boolean;
     button: Schema.Attribute.Component<'list.buttons', true>;
     intro_text: Schema.Attribute.RichText &
-      Schema.Attribute.CustomField<'plugin::ckeditor.CKEditor'>;
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor.CKEditor',
+        {
+          licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3ODE5MTM1OTksImp0aSI6IjAwNzQ3NzFlLTljMTEtNDZiMC04MDNkLWQ0NDc4Y2RlZjI1NiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCIsIkUyUCIsIkUyVyJdLCJ2YyI6ImU2YWIwMzhhIn0.GpIsNENbfZ9D4_CL1Go1xs7h8ycVnXq-rrQLj3c-h4do2kxO5s3S7PfPSn0J2Yg-01PmRsSmyuFPUCB4zakwAQ';
+          output: 'HTML';
+          preset: 'rich';
+        }
+      >;
   };
 }
 
